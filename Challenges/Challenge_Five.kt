c3a2f499b54f85cc1feddbd2e573bf64857f2eb7
Akebu <74776297+akebu6@users.noreply.github.com>
2022-07-10 23:11:32 +0200

    [code]: added challenge five solution

--- Challenges/Challenge_Five.kt
+++ Challenges/Challenge_Five.kt
@@ -1,6 +1,34 @@
 /*
 * QUESTION
-*
+* There is a function suspiciousFunction, and for some reason you suspect that an exception might occur.
+* Process the call of suspiciousFunction with the help of the try-catch-finally statement. If an exception occurs in the function, print an information message about it.
+* In the case of IOException, print the following message instead: The IOException occurred.
+* In the final block, output Handling completed successfully!.
 */
 
 // SOLUTION
+import java.io.IOException
+import java.lang.ArithmeticException
+
+fun suspiciousFunction(param: Int) {
+    when (param) {
+        0 -> throw Exception("Some exceptions?")
+        1 -> throw ArithmeticException("Division by zero")
+        2 -> throw Exception("An exception occurred here")
+        3 -> throw IOException()
+    }
+}        
+
+fun handleException(data: Int) {
+    try {
+        suspiciousFunction(data)
+    } catch (e: IOException) {
+        println("The IOException occurred")
+    } catch (e: Exception) {
+        println(e.message)
+    } finally {
+        println("Handling completed successfully!")
+    }
+}
+
+
