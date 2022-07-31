c246a18abd901ec0e656fab244a62d08ce3a98d1
Akebu <74776297+akebu6@users.noreply.github.com>
2022-07-31 19:46:24 +0200

    [docs(code)]: added challenge ten

---
+++ Challenges/Challenge_Ten.kt
@@ -0,0 +1,10 @@
+/******************************************************************************************************************
+* Make an object Me that store the following constants: CURRENT_AGE = 18, EYES_COLOR = green, and HEIGHT = 188.
+******************************************************************************************************************/
+
+// SOLUTION
+object Me {
+    const val CURRENT_AGE = 18
+    const val EYES_COLOR = "green"
+    const val HEIGHT = 188
+}
