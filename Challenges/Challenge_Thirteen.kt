e5ca609413a9f95ff8ad68f889f5d31e52e7b429
Akebu <74776297+akebu6@users.noreply.github.com>
2022-08-11 07:12:43 +0200

    chore: added new challenge question

---
+++ Challenges/Challenge_Thirteen.kt
@@ -0,0 +1,6 @@
+/*********************************************************************************************************************************************************
+* QUESTION:
+* The password is hard to crack if it contains at least A uppercase letters, at least B lowercase letters, at least C digits, and includes exactly N symbols. Also, a password cannot contain two or more same characters coming one after another. For a given numbers A, B, C, N you should output a password that matches these requirements.
+*
+* It is guaranteed A, B, C, and N are non-negative integers and A + B + C <= N. Keep in mind, that any parameter can be equal to zero. It means that it's ok if the password doesn't contain symbols of such type.
+**********************************************************************************************************************************************************/
