a4323ef9a7a5366bba8c3acbc0528fcdba0b4cfd
Akebu <74776297+akebu6@users.noreply.github.com>
2022-08-18 21:55:37 +0200

    [chore]: added new question

---
+++ Challenges/Challenge_Fourteen.kt
@@ -0,0 +1,5 @@
+/********************************************************************************************************************************************************************
+* Write a program that prints a part of the following sequence 1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 ... (the number of repetitions is equal to the respective number). Input is a positive integer n – the number of the elements of the sequence that the program should print. Output the sequence of numbers, written in a single line, separated by spaces.
+*
+* For example, if n is 7, the program should output 1 2 2 3 3 3 4 (seven numbers).
+*****************************************************************************************************************************************************************/
