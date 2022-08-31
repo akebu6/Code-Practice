bbc90c542f9dc0c1c358883c09037ca3bd15508e
Akebu <74776297+akebu6@users.noreply.github.com>
2022-08-31 10:29:38 +0200

    [chore]: added challenge question

---
+++ Challenges/Challenge_Sixteen.kt
@@ -0,0 +1,21 @@
+/*******************************************************************************************************************
+* QUESTION:
+* Read the input string and duplicate it in the file MyFile.txt.
+* Examples:
+*
+* 1) Input string:
+*
+* Yohoho!
+*
+* After we have launched the code:
+*
+* Yohoho!Yohoho!
+*
+* 2) Input string:
+*
+* meow
+*
+* After we have launched the code:
+*
+* meowmeow
+*********************************************************************************************************************/
