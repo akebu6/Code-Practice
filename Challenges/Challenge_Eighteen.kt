4eeec4f0b8dc3bbdd218e112416af1723f0250b0
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-13 07:25:09 +0200

    [CHORE]: added solution

--- Challenges/Challenge_Eighteen.kt
+++ Challenges/Challenge_Eighteen.kt
@@ -10,3 +10,20 @@
 * Output format:
 * A sequence of integers in a single line, separated by spaces.
 *****************************************************************************************************************************************/
+
+// SOLUTION
+fun main() {
+    val numbers: MutableList <Int> = mutableListOf()
+    var num = readln().toInt()
+    print("$num ")
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2
+            numbers.add(num)
+        } else {
+            num = num * 3 + 1
+            numbers.add(num)
+        }        
+    }
+    println(numbers.joinToString(" "))
+}
