e6c875ba0336042cbff79d0a102ff9a210262523
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-15 21:02:29 +0200

    [chore]: added solution
    closes issue #14

--- Challenges/Challenge_Nineteen.kt
+++ Challenges/Challenge_Nineteen.kt
@@ -4,3 +4,15 @@
 ***********************************************************************************************************************************/
 
 // SOLUTION
+data class Student(var name: String, var money: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+      
+        other as Student
+        if (name == other.name && money < 1500) return true
+        
+        return false
+    }
+
+}
