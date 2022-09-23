22432a7c7f26f9f612bbf6fb646fd1fb227d7850
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-23 06:19:05 +0200

    [chore]: closes #16

---
+++ Challenges/Challenge_Twenty_One.kt
@@ -0,0 +1,11 @@
+/**************************************************************************************************************************************************************
+* Write a program that reads a string s and an int n, and then moves the first n characters of s to the end of the string. The program must output the changed string. 
+* If n is greater than the length of the string, it must output the string unchanged.
+*
+* Note, the substring() method can help you, but you may choose another way to solve the problem.
+* 
+* Input data format
+* The single input line contains s and n separated by a space.
+******************************************************************************************************************************************************************
+
+// SOLUTION
