4ac8d2eb34e97d2f1a6f9d86ece76aadc672dcdb
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-24 23:27:05 +0200

    [chore]: closes #17

---
+++ Challenges/Challenge_Twenty_Two.kt
@@ -0,0 +1,13 @@
+/********************************************************************************************************************************************************
+* QUESTION
+* The detective has a file with all successfully completed cases and the name of each new case begins with a new line.
+*
+* Recently, he successfully solved a case he called "The Sign of the Four".
+*
+* Write code to add the new case called "The Sign of the Four" into the file. The file name is MyCases.txt. Note that the text should be on the new line.
+*
+* Please, don't remove the variable myFile, we need it to check your answer
+********************************************************************************************************************************************************/
+
+// SOLUTION
+
