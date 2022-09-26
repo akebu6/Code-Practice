8927c509794d0ae74de469d044fc890c9d21b21a
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-26 21:10:50 +0200

    [chore]: added challenge 23

---
+++ Challenges/Challenge_Twenty_Three.kt
@@ -0,0 +1,6 @@
+/*************************************************************************************************************************************************
+* QUESTION
+* Imagine that you want to play the "Rock-Paper-Scissors" game and you need to write a function makeDecision() that returns Rock, Paper, or Scissors depending on a random number from 1 to 3.
+*
+* Write that function.
+**************************************************************************************************************************************************/
