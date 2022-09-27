021db0be028bab489cb6f43d8982d13982e68480
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-27 20:47:07 +0200

    [chore]: added challenge 24

---
+++ Challenges/Challenge_Twenty_Four.kt
@@ -0,0 +1,10 @@
+/************************************************************************************************************************
+* QUESTION
+* Write a function solution that reads two numbers from the input and prints the first number divided by the second.
+*
+* If an exception occurs, print the exception's message.
+*
+* In the final block, the output will be This is the end, my friend.
+*************************************************************************************************************************/
+
+// SOLUTION
