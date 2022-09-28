146087ea9717f7b7bf72324ee97a6137f9e9db20
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-28 20:29:20 +0200

    [chore]: added challenge 25

---
+++ Challenges/Challenge_Twenty_Five.kt
@@ -0,0 +1,9 @@
+/*********************************************************************************************************************************************
+* QUESTION
+* Imagine that you're a tutor. You need to write a program that parses your students' answers and prints true if the answer is I can do my homework on time! or I can't do my homework on time! and false in all other cases.
+*
+* To complete this task, use the function matches().
+*********************************************************************************************************************************************/
+
+// SOLUTION
+
