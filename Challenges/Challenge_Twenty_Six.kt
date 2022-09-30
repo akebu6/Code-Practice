f97575c346ae0f85fcee05869cb255541b5b3571
Akebu <74776297+akebu6@users.noreply.github.com>
2022-09-30 23:56:48 +0200

    chore: added solution

--- Challenges/Challenge_Twenty_Six.kt
+++ Challenges/Challenge_Twenty_Six.kt
@@ -4,3 +4,10 @@
 ************************************************************************************************************************************************************/
 
 // SOLUTION
+fun main() {
+    // write here
+    val input = readln()
+    val intValue = input.toInt()
+    val result = intValue / 2
+    println("The obtained value is $intValue and its Int representation is $result")
+}
