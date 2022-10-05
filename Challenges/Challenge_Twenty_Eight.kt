965aeefc45b5982ec3a6e1dc98255ad6bbdca96b
Akebu <74776297+akebu6@users.noreply.github.com>
2022-10-05 22:35:21 +0200

    chore: added new challenge

---
+++ Challenges/Challenge_Twenty_Eight.kt
@@ -0,0 +1,8 @@
+/***********************************************************************************************************************************************************
+* QUESTION
+* Write a class Labrador that inherits from a class Dog(name: String).
+*
+* The Labrador class has one method called printInfo(), which prints The dog's name is %DOGS_NAME%, his color is %DOGS_COLOR% and his weight is %DOGS_WEIGHT%.
+**************************************************************************************************************************************************************/
+
+// SOLUTION
