25bc8196343e78455a9f2fa380b2d2a6690efcc8
Akebu <74776297+akebu6@users.noreply.github.com>
2022-10-06 08:27:32 +0200

    [chore]: added solution

--- Challenges/Challenge_Twenty_Eight.kt
+++ Challenges/Challenge_Twenty_Eight.kt
@@ -6,3 +6,10 @@
 **************************************************************************************************************************************************************/
 
 // SOLUTION
+class Labrador(val name: String, val color: String, val weight: Int) : Dog(name) {
+    fun printInfo() {
+        if (name != "Kiki") {
+            println("The dog's name is $name, his color is $color and his weight is $weight")
+        }
+    }
+}
