42381905bf9c986f8522eb54d4c31366addf33f7
RohitLakra299 <rohitlakra299@gmail.com>
2022-10-20 10:07:15 +0530

    Added solution to challenge 31

---
+++ Solutions/solution_thirtyOne_kotlin.kt
@@ -0,0 +1,16 @@
+ fun main() {
+        var maxi =0
+        var input = readln().toInt()
+        while(input != 0){
+            if(input > maxi){
+                maxi = input
+            }
+            input = readln.toInt()
+        }
+        println(maxi)
+        }
+        
+        
+     
+         
+     
