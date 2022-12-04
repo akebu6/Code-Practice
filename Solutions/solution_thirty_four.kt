7d7ca68e8c675d8e967dfad02e2282a2f4efc8e4
Akebu <74776297+akebu6@users.noreply.github.com>
2022-12-04 17:15:09 +0200

    chore: added solution for challenge 34
    resolved #46

---
+++ Solutions/solution_thirty_four.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val partOneOfString = "I"
+    val partTwOfString = "'"
+    val partThreeOfString = " learning Kotlin"
+     
+    repeat(6) { println("$partOneOfString$partTwoOfString$partThreeOfString")
+}
+    
