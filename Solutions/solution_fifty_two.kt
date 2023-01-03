ce972a603e597e8075bb16a3cf32c3d78ec010bb
Akebu <74776297+akebu6@users.noreply.github.com>
2023-01-03 11:10:11 +0200

    chore: added solution 52
    resolved #74

---
+++ Solutions/solution_fifty_two.kt
@@ -0,0 +1,19 @@
+fun pepTalk(name: String): String {
+    val array = name.split(" ").toTypedArray()
+    val firstName = array[0]
+    val secondName = array[1]
+    return "Don't lose the towel, traveler $firstName $secondName!"
+}
+
+fun main() {
+    val name = readdLine()!!
+    println(
+        try {
+            pepTalk(name)
+        } catch(e: Exception) {
+            "Don't lose the towel, nameless one."
+        } finally {
+            println("Good luck!")
+        }
+    )
+}
