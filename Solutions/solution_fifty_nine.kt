1aec4504a3c1cede089f59fc0cdb569c1e537529
Akebu <74776297+akebu6@users.noreply.github.com>
2023-01-26 20:49:59 +0200

    chore: added solution 59
    resolved #89

---
+++ Solutions/solution_fifty_nine.kt
@@ -0,0 +1,8 @@
+open class Animal
+open class Mammal : Animal()
+class Lion : Mammal()
+open class Fish : Animal()
+class Salmon : Fish()
+class Carp : Fish()
+open class Bird : Animal()
+class Eagle : Bird()
