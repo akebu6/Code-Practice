e33615c614601ed9b2ab1a01477ae4565329896b
Akebu <74776297+akebu6@users.noreply.github.com>
2023-02-10 20:38:49 +0200

    task: added solution for challenge 60

---
+++ Solutions/solution_sixty.kt
@@ -0,0 +1,8 @@
+class Me() {
+    companion object Companion {
+        val myBrother = "Michael"
+    }
+}
+
+// the line of code that will print the contents in myBrother
+println(Me.Companion.myBrother)
